70dbf37b83091f41ca207ed25a92aedc7489b2e1
unknown <muhajirhayru18@gmail.com>
2025-10-16 02:55:15 -0700

    the admin is changed

--- ayn/urls.py
+++ ayn/urls.py
@@ -21,6 +21,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('cake.urls')),
-    path('admin/', admin.site.urls),
+    path('controll/', admin.site.urls),
 ]   # only for development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
